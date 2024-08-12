cd9cd984afc12e6e4366f1cfd88baea818ceb94f
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:18:53 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Code Along Norms: 
+// 1. Only be on Repl.it/classroom resources
+// 2. Actively follow along and engage
+// 3. Ask questions and flag if you get stuck
+// 4. Take notes as needed!
+let name = prompt("What's your name?")
+//With a + sign, need to add a space
+console.log("Welcome to my game, " + name)
+//With a , sign, spaces added automatically
+console.log("Welcome to my game,", name)
+//With backticks, you can input variables directly
+console.log(`Welcome to my game, ${name}`)
+
+let role = prompt("what role do you want? (magician, fighter, peasant)")
+console.log(`Okay, ${name}, your sole is now ${role}.`)
